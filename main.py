5c545285b8981210b640d37cf146662aa57038ce
Pussymonsta <fominok@hotmail.com>
2015-06-03 01:21:17 +0000

    File input, [A] matrix length, test it.

--- main.py
+++ main.py
@@ -1,10 +1,42 @@
+import sys
+
 from parser import Parser
 import spicemix
 
 
-def main():
-    pass
+def find_n_m_size(comp_list):
+
+    """Function to get the size of matrix (N + M)"""
+
+    max_value = 0
+    voltage_number = 0
+
+    for comp in comp_list:
+        p = comp.get_p_node()
+        n = comp.get_n_node()
+        max = p if p > n else n
+        if max > max_value:
+            max_value = max
+        if isinstance(comp, spicemix.Voltage):
+            voltage_number += 1
+
+    return max_value + voltage_number + 1
+
+
+def main(args):
+    if len(args) != 2:
+        print('Usage: python ' + args[0] + ' input.txt')
+        sys.exit()
+
+    parser = Parser()
+    components = []
+
+    with open(args[1]) as file:
+        for line in file:
+            comp = parser.next_entry(line)
+            if comp is not None:
+                components.append(comp)
 
 
 if __name__ == '__main__':
-    main()
+    main(sys.argv)
--- test.py
+++ test.py
@@ -2,6 +2,8 @@ import unittest
 import parser
 import spicemix
 
+from main import find_n_m_size
+
 
 class ParserTests(unittest.TestCase):
     def setUp(self):
@@ -16,14 +18,14 @@ class ParserTests(unittest.TestCase):
                         (spicemix.Voltage, 1, 1, 0, 2),
                         'I1 2 3 0.3':
                         (spicemix.Current, 1, 2, 3, 0.3),
-                        'V12 2 3 -0.3':
-                        (spicemix.Voltage, 12, 2, 3, -0.3),
                         }
+        self.nm_matrix_len = 5
                        
 
         self.parser = parser.Parser()
 
     def test_correct_type_nodes_anything(self):
+        comps = []
         for entry, value in self.entries.items():
             comp = self.parser.next_entry(entry)
 
@@ -31,11 +33,14 @@ class ParserTests(unittest.TestCase):
                 raise Exception('There is a component but parser returns None')
 
             if comp is not None:
+                comps.append(comp)
                 assert isinstance(comp, value[0])
                 self.assertEqual(comp.get_id(), value[1], 'Wrong id')
                 self.assertEqual(comp.get_p_node(), value[2], 'Wrong p node')
                 self.assertEqual(comp.get_n_node(), value[3], 'Wrong n node')
                 self.assertEqual(comp.get_value(), value[4], 'Wrong value')
+
+        self.assertEqual(find_n_m_size(comps), self.nm_matrix_len)
 
 
 if __name__ == '__main__':
